b9845807746d2796a6c7357f36ed8740e1e358dd
weijinyan <weijinyan@live.com>
2020-01-06 10:45:43 +0800

    update sleep time for test

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -87,7 +87,7 @@ class OwnerController {
 
         // find owners by last name
         Collection<Owner> results = this.owners.findByLastName(owner.getLastName());
-		Thread.sleep(10000);
+		Thread.sleep(20000);
         if (results.isEmpty()) {
             // no owners found
             result.rejectValue("lastName", "notFound", "not found");
